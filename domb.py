60328b2c80e74f50e05045a438cbb89f69ac41f0
Csiky-has-long <csik.adam.jozsef@diak.szbi-pg.hu>
2025-03-26 14:51:44 +0100

    personalised big boobs

---
+++ domb.py
@@ -0,0 +1,22 @@
+obet = "O "
+for i in range(1, ):
+    print(obet* i)
+for i in range(5,0,-1):
+    print(obet* i)
+
+print("hegyes csöcsű")
+hanyszor = 6
+obet = "O "
+for i in range(1, hanyszor ):
+    print(obet* i)
+for i in range(hanyszor,0,-1):
+    print(obet* i)
+    
+print("személyre szabott kecske csöcsű")
+hanyszor=int(input("Mekkora csöcs ke'? "))
+obet = "O "
+for i in range(1, hanyszor ):
+    print(obet* i)
+for i in range(hanyszor-1,0,-1):
+    print(obet* i)
+
---
+++ for_ciklus.py
@@ -0,0 +1,34 @@
+#irass ki egymás után 10 számot!
+# for szam in range(10):
+#     print(szam)
+
+#irass ki egymás után 1-10 számokat!
+# for szam2 in range(1,11):
+#     print(szam2)
+
+#irass ki egymás után 1-10ig a páros számokat!
+# for szam2 in range(1,11):
+#     if szam2 %2 ==0:
+#         print(szam2)
+
+
+# for szam2 in range (2,11,2):
+#     print (szam2)
+    
+# for szam2 in range(1,11):
+#     if szam2 %2 ==0:
+#         print(szam2)
+
+
+#irass ki egymás után 10-1ig a számokat!
+# for szam2 in range(10,0,-1):
+#     print(szam2)
+
+obet = "O"
+for i in range(1,5):
+    print(obet* i)
+for i in range(5,0,-1):
+    print(obet* i)
+
+    
+
